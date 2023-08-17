05b3f1f21591539e61115adeeff2130cf7be389c
Jack Tran <bluestar113311@gmail.com>
2023-08-17 14:59:59 -0700

    Add event handling for tweet character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function () {
+    $countdown = 140 - $(this).val().length;
+    
+    $counter = $(this).closest("form").find(".counter")
+    $counter.text($countdown);
+
+    if($countdown < 0) {
+      $counter.css('color', 'red')
+    } else {
+      $counter.css('color', 'black')
+    }
+    $(".counter").text($countdown);
+  });
+});
