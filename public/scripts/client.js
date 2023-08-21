98b2d031afb07db6d6a438078381d44aed47627b
Jack Tran <bluestar113311@gmail.com>
2023-08-21 00:35:19 -0700

    Add prevent XSS with escaping

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+const escaper = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const renderTweets = function(tweets) {
   for(let i = 0; i < tweets.length; i++) {
@@ -46,7 +26,7 @@ const createTweetElement = function(tweet) {
             </div>
             <span>${tweet.user.handle}</span>
           </header>
-          <div class="tweet">${tweet.content.text}</div>
+          <div class="tweet">${escaper(tweet.content.text)}</div>
           <footer>
             <span>${timeago.format(tweet.created_at)}</span>
             <div class="options">
